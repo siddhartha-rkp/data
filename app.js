79128469e42b88d97a640940067a143120ff32cd
siddhartha-rkp <skpandey0002@gmail.com>
2019-10-10 11:36:39 +0530

    Test
    Test

---
+++ app.js
@@ -0,0 +1,13 @@
+const flightPath = {
+  curviness: 1.25,
+  autoRotate: true,
+  values: [{ x: 100, y: -20 }]
+};
+
+const tween = new TimelineLite();
+tween.add(
+  TweenLite.to(".tube", 1, {
+    bezier: flightPath,
+    ease: Power1.easeInOut
+  })
+);
